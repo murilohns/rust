1f5b7a8088883458df94753ca8c9d0eac76ae86e
Murilo Henrique <murilohns@gmail.com>
2018-12-27 17:43:14 -0200

    rectangle: add can_hold method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,26 +1,39 @@
-//Derive is used to allow the macro println! to print the struct
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
-    heigth: u32,
+    height: u32,
 }
 
 impl Rectangle {
     fn area(&self) -> u32 {
-        self.width * self.heigth
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rectancle: &Rectangle) -> bool {
+        self.width > rectancle.width && self.height > rectancle.height
     }
 }
 
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
-        heigth: 50,
+        height: 50,
     };
 
-    println!("The rectangle is: {:#?}", &rect);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
-        &rect.area()
+        &rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
